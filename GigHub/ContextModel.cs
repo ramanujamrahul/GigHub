28d755a50955852cd8c915bdce5ce5239d1984aa
rahul ramanujam <ramanujamrahul@gmail.com>
2020-11-09 23:49:51 -0500

    added identioty

---
+++ GigHub/ContextModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GigHub
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+    }
+}
